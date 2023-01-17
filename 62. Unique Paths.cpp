2cb2beb74b5499126316dabf7b1cd77e70774649
Tanveer Ahmed Nirjhor <36777480+nirjhor01@users.noreply.github.com>
2023-01-17 15:36:15 +0600

    Create 62. Unique Paths.cpp

---
+++ 62. Unique Paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int dp[m + 1][n + 1];
+        for(int i = 0; i < m; ++i)
+        for(int j = 0; j < n; ++j)
+        dp[i][j] = 0;
+
+        dp[0][0] = 1;
+        for(int i = 0; i < m; ++i)
+        for(int j = 0; j < n; ++j){
+            if(i + j == 0) continue;
+            if(i - 1 >= 0) dp[i][j] = dp[i][j] + dp[i - 1][j];
+            if(j - 1 >= 0) dp[i][j] = dp[i][j] + dp[i][j - 1];
+        }
+        return dp[m - 1][n - 1];
+    }
+};
