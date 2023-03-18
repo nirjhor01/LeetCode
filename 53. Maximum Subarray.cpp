e3aeab8dcd59322929a975e5ee54b8fa9f8c782d
Tanveer Ahmed Nirjhor <36777480+nirjhor01@users.noreply.github.com>
2023-03-18 22:21:50 +0600

    Update 53. Maximum Subarray.cpp

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -1,19 +1,13 @@
-#KADAN's algo
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int mx = 0;
+        int mx = INT_MIN;
         int sum = 0;
-        int neg = 0;
         for(auto i: nums){
-            neg +=(i < 0);
             sum += i;
-            if(sum < 0) sum = 0;
             mx = max(sum, mx);
-        }
-        if(neg == nums.size()){
-            sort(nums.begin(),nums.end());
-            return nums[nums.size()-1];
+            if(sum < 0) sum = 0;
+            
         }
         return mx;
     }
